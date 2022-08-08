73a7921627edf6333f9346f134ef7d1f4a3b30c0
egetelli <egetelli@hotmail.com>
2022-08-08 09:00:22 +0300

    Add project files.

---
+++ MVCOrnek/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCOrnek.MvcApplication" Language="C#" %>
---
+++ MVCOrnek/Models/customervalidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCOrnek.Models
+{
+    [MetadataType(typeof(customerMetaData))]
+    public partial class customer
+    {
+        public class customerMetaData
+        {
+            [DisplayName("Customer Name")]
+            public string custname { get; set; }
+            [DisplayName("Address")]
+            public string address { get; set; }
+            [DisplayName("Mobile No")]
+            public Nullable<int> mobile { get; set; }
+        }
+    }
+}
